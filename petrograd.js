e88dd6b2673ff80637d9d8df73217cd691badb1c
Simone Drescher <simonedrescher@Simones-MacBook-Pro.local>
2017-02-23 12:03:47 +0100

    add data navn, pris and rabatpris

--- petrograd.js
+++ petrograd.js
@@ -2,13 +2,28 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises(){
     console.log("siden vises");
-    visProdukt();
+
+    // læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
 }
 
-function visProdukt(){
+function visProduktListe( listen ){
+    console.table( listen );
+    listen.forEach(visProdukt);
+}
+
+
+function visProdukt( produkt ){
+    console.log( produkt );
     // klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     // indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris*produkt.rabatsats/100));
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
     // append klon i .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
